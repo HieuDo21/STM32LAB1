bdb43557421eb8b2f5489557fa063d9288804523
Hieu Do <hieu.do2109@hcmut.edu.vn>
2023-10-03 01:09:22 +0700

    finish ex8

--- STM32_PROJECT/Core/Src/main.c
+++ STM32_PROJECT/Core/Src/main.c
@@ -58,6 +58,9 @@ void setNumberOnClock(int num){
 	uint16_t currentPin = GPIO_PIN_0 << (4+num);
 	HAL_GPIO_WritePin(GPIOA, currentPin, SET);
 }
+void clearAllClock () {
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15, RESET);
+}
 /* USER CODE END 0 */
 
 /**
@@ -102,6 +105,8 @@ int main(void)
 	if(num <= 11){
 		setNumberOnClock(num);
 		num++;
+	}else{
+		clearAllClock();
 	}
 	HAL_Delay(1000);
   }
